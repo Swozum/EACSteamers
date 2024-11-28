fb035fbf3f4fdcf228322cf99a157f9e53d662f3
Nathaniel Aguiar Blair <106711209+Swozum@users.noreply.github.com>
2024-11-28 12:18:43 +0000

    Add files via upload

---
+++ EACSteamers/photos.js
@@ -0,0 +1,42 @@
+const photos = [
+    "img/Steamers1.jpg"
+];
+const cards = [
+    card1 = {
+        name : 'Foto dos herois ferroviarios',
+        photo: 'img/Steamers1.jpg',
+        text :{
+            title:'Flag Football no Entroncamento',
+            subtitle:'Nova equipe de futebol americano no entroncamento',
+            text:'A partir de 2025 a cidade do Entroncamento ira ter um representante no campeonato nacional de Flag Football'
+        }
+
+    },
+    card2 = {
+        name : 'Foto dos herois ferroviarios',
+        photo: '"img/Steamers1.jpg"',
+        text :{
+            title:'Flag Football no Entroncamento',
+            subtitle:'Nova equipe de futebol americano no entroncamento',
+            text:'A partir de 2025 a cidade do Entroncamento ira ter um representante no campeonato nacional de Flag Football'
+        }
+
+    },
+    card3 = {
+        name : 'Foto dos herois ferroviarios',
+        photo: '"img/Steamers1.jpg"',
+        text :{
+            title:'Flag Football no Entroncamento',
+            subtitle:'Nova equipe de futebol americano no entroncamento',
+            text:'A partir de 2025 a cidade do Entroncamento ira ter um representante no campeonato nacional de Flag Football'
+        }
+
+    },
+];
+const videos = [
+    video1 = {
+        name:'',
+        path:'',
+        shortText:''
+    }
+];
